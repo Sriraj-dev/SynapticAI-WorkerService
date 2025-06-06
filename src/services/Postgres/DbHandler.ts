f045fbd093ce52fa6f6b536be7aa4fc4d2fe787d
Sriraj Palakurthi <srirajdev@Srirajs-Laptop.local>
2025-06-06 19:43:39 +0530

    Added User Usage Metrics

--- src/services/Postgres/DbHandler.ts
+++ src/services/Postgres/DbHandler.ts
@@ -1,6 +1,7 @@
-import { eq, InferInsertModel, sql } from "drizzle-orm"
+import { AnyColumn, eq, type InferInsertModel, sql } from "drizzle-orm"
 import { db } from "./connectToPG"
-import { notes, NoteStatusLevel, semanticNotes } from "./schema"
+import { notes, NoteStatusLevel, NoteStatusReason, semanticNotes, SubscriptionTier, userUsageMetrics } from "./schema"
+import type { NewUserUsageMetrics, UserUsageMetrics } from "../../utils/models"
 
 export const DBHandler = {
 
@@ -13,7 +14,7 @@ export const DBHandler = {
         try{
             const inserted = await db.insert(semanticNotes).values(records).returning()
 
-            console.log(`✅ Inserted ${inserted.length} chunk embeddings for note ${records[0].noteId}`)
+            console.log(`✅ Inserted ${inserted.length} chunk embeddings for note ${records[0]?.noteId}`)
         }catch(err){
             console.error("❌ Error in inserting chunk embeddings", err)
             throw err;
@@ -31,14 +32,67 @@ export const DBHandler = {
         }
     },
 
-    async updateNoteStatus(status : NoteStatusLevel, noteId: string){
+    async updateNoteStatus(status : NoteStatusLevel,noteId: string,reason? : NoteStatusReason){
         try{
-            await db.update(notes).set({status}).where(eq(notes.uid, noteId))
+            const note = await db.select().from(notes).where(eq(notes.uid, noteId)).limit(1);
+
+            if (note.length === 0) {
+                console.warn(`⚠️ Note with ID ${noteId} not found. Skipping update.`);
+                return;
+            }
+
+            await db.update(notes).set({status: status, reason: reason}).where(eq(notes.uid, noteId))
 
             console.log(`✅ Updated note status for note ${noteId} to ${status}`)
         }catch(err){
             console.error("❌ Error in updating note status", err)
             throw err
+        }
+    },
+
+    async getUserUsageMetrics(userId: string) : Promise<UserUsageMetrics | null> {
+        try{
+           const userMetrics = await db.select().from(userUsageMetrics).where(eq(userUsageMetrics.userId, userId))
+
+           return userMetrics.length > 0 ? userMetrics[0] : null;
+           
+        }catch(err){
+            console.log("Unable to get the user usage metrics from neon database : ", err)
+            return null
+        }
+    },
+
+    async createUserUsageMetrics(userId: string, subscription_tier : SubscriptionTier) : Promise<UserUsageMetrics> {
+        try{
+            const newMetrics: NewUserUsageMetrics = {
+                userId: userId,
+                //TODO: Set the limit values based on the subcription tier.
+            };
+
+            const metrics = await db.insert(userUsageMetrics).values(newMetrics).returning();
+            return metrics[0];
+        }catch(err){
+            console.log("Unable to create user usage metrics in neon database : ", err);
+            throw err;
+        }
+    }, 
+
+    async incrementUserUsageMetrics(userId : string, embedded_tokens_used : number = 0) : Promise<UserUsageMetrics> {
+        try{
+            const increment = (column: AnyColumn, value = 1) => {
+                return sql`${column} + ${value}`;
+            };
+
+            const userMetrics = await db.update(userUsageMetrics).set({
+                total_embedded_tokens: increment(userUsageMetrics.total_embedded_tokens, embedded_tokens_used),
+                updatedAt: new Date(),
+            })
+            .where(eq(userUsageMetrics.userId, userId)).returning()
+            
+            return userMetrics[0];
+        }catch(err){
+            console.log("Unable to increment user usage metrics in neon database : ", err);
+            throw err;
         }
     }
 }
--- src/utils/utility_methods.ts
+++ src/utils/utility_methods.ts
@@ -1,6 +1,16 @@
+import {encode} from 'gpt-tokenizer';
 
 export function logMemory(label: string) {
   const mem = process.memoryUsage()
   const mb = (bytes: number) => (bytes / 1024 / 1024).toFixed(2) + 'MB'
   console.log(`[${label}] Memory - RSS: ${mb(mem.rss)}, HeapUsed: ${mb(mem.heapUsed)}, HeapTotal: ${mb(mem.heapTotal)}`)
+}
+
+export function estimateTokens(text: string): number {
+  try {
+    return encode(text).length;
+  } catch (error) {
+    console.error("Error estimating tokens:", error);
+    throw error;
+  }
 }
